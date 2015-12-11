1a24be7cd542c77d856664878fee84d66cdf675c
Matt <matthew.n.ferguson@gmail.com>
2015-12-11 01:30:52 +0100

    tweak

--- app/index.jsx
+++ app/index.jsx
@@ -3,18 +3,12 @@ import './main.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
+import Home from './components/Home.jsx';
 import Parse from 'parse';
 
 Parse.initialize("2ORhMZvPcIVIQXCKRevAcDKKB3qTKdISH1s7kunP", "EVbY8lGJqEfzDkSceJ8qZDRbeSrpfGMy2hbRkZOH");
 
-// Launch hoisted functions and variables below
-main();
+// WE ONLY NEED THIS TO IMPORT APP
 
-function main() {
-
-  // Create a div called app for App.jsx to be rendered into
-  const app = document.createElement('div');
-  document.body.appendChild(app);
-  // Render App.jsx into
-  // ReactDOM.render(<App />, app);
-}
+// Render App.jsx into
+// ReactDOM.render(<App />, app);
