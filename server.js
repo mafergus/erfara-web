c7bdeea487b19ede8c22122750d7f61c768b39ea
matt-thinair <matt@thinair.com>
2016-12-27 18:43:25 -0800

    Fixing function usage in server.js

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 var express = require('express');
-
 var server=express();
-
-
 var port = process.env.PORT || 3000;
 
-server.use(express.static(__dirname + '/build'));
+// server.use(express.static(__dirname("") + "/build"));
 
-server.get('/', function(req,res) {
- res.send('./build/index.html');
+server.get("/", function(req,res) {
+ res.send("./build/index.html");
 });
 
 server.listen(port);
