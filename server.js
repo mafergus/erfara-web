4edb5600bd491b24141b8f826dc9396cac32f2c9
mafergus <matthew.n.fergusom@gmail.com>
2017-01-12 20:50:21 -0800

    asd

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 var express = require('express');
+const path = require('path');
 var server=express();
 var port = process.env.PORT || 3000;
 
 // server.use(express.static(__dirname("") + "/build"));
 
 server.get("/", function(req,res) {
- res.send("/build/index.html");
+ res.sendFile(path.join(__dirname + "/build/index.html"));
 });
 
 server.listen(port);
